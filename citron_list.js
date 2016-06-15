d7b4b659b472e546fb691c9eab47b605caa3a75a
matt lucas <mattlucas@gmail.com>
2016-06-15 13:44:04 -0400

    social sharing

--- citron_list.js
+++ citron_list.js
@@ -24,6 +24,25 @@ if (Meteor.isClient) {
 		}
 	});
 
+  ShareIt.configure({
+    sites: {                // nested object for extra configurations
+        'facebook': {
+            'appId': 1032542676825405 // use sharer.php when it's null, otherwise use share dialog
+        },
+        'twitter': {},
+        'googleplus': {},
+        'pinterest': {}
+    },
+    classes: "large btn", // string (default: 'large btn')
+                          // The classes that will be placed on the sharing buttons, bootstrap by default.
+    iconOnly: true,      // boolean (default: false)
+                          // Don't put text on the sharing buttons
+    applyColors: true,     // boolean (default: true)
+                          // apply classes to inherit each social networks background color
+    faSize: '',            // font awesome size
+    faClass: ''       // font awesome classes like square
+  });
+
 
 	// Template.mainLayout.events({
 	// 	"keyup li.search input": function(event){
