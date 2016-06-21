f75b34da2cd33bcf3d0ea9bc85d9f6b6155d4015
matt lucas <mattlucas@gmail.com>
2016-06-21 17:57:26 -0400

    messing with the db

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+// ServiceConfiguration.configurations.remove({
+//     service: 'facebook'
+// });
+ 
+// ServiceConfiguration.configurations.insert({
+//     service: 'facebook',
+//     appId: '12345678901234567890',
+//     secret: ''
+// });
